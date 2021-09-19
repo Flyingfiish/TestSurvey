47c5423aefe1a5c9b204fec3a76907dfe6cb7b07
eldar <eldarg76@gmail.com>
2021-09-19 13:26:23 +0500

    ///

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+import "../styles/questions.css"
 import '../styles/globals.css'
 import "../styles/header.css"
-import "../styles/question.css"
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-  
+
 }
 
 export default MyApp
