2610927b8c2d7376fa673d2e2a8724b2d4ea3499
eldar <eldarg76@gmail.com>
2022-08-12 14:41:58 +0500

    Autofaq

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-document-import-in-page */
+/* eslint-disable @next/next/no-sync-scripts */
+import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
+
+export default function Document() {
+	return (
+		<Html>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+				<link href="https://chat.autofaq.ai/widget/static/css/main.css" rel="stylesheet" />
+				<script
+					data-widget-host="https://chat.autofaq.ai"
+					data-widget-service-id="1c3476b7-4cd9-41cc-aec3-096858431fff"
+					data-widget-channel-id="e7f927e4-b205-48e5-ae17-467761056ef2"
+					data-widget-user-login="default"
+					data-widget-user-name=""
+					data-widget-user-email=""
+					src="https://chat.autofaq.ai/widget/static/js/main.js"
+					id="autofaqWidget"
+				></script>
+			</body>
+		</Html>
+	);
+}
