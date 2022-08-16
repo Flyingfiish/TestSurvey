65430aa80d948bc5b4c68bf8da91af69c8f16876
eldar <eldarg76@gmail.com>
2022-08-16 14:28:28 +0500

    Add fake register

--- pages/index.js
+++ pages/index.js
@@ -6,56 +6,69 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
 const questions = [
-  {
-    text: "Как вы относитесь к тому, что на улице идет дождь?",
-    variants: [
-      {
-        
-        text: "Позитивно",
-
-      },
-      {
-        text: "Негативно",
-      },
-    ],
-    numberInfo: { index: 1, length: 20 },
-  },
-  {
-    text: "Вам сейчас холодно?",
-    variants: [
-      {
-        text: "Да",
-      },
-      {
-        text: "Нет",
-      },
-    ],
-    numberInfo: { index: 1, length: 20 },
-  },
+	{
+		text: "Как вы относитесь к тому, что на улице идет дождь?",
+		variants: [
+			{
+				text: "Позитивно"
+			},
+			{
+				text: "Негативно"
+			}
+		],
+		numberInfo: { index: 1, length: 20 }
+	},
+	{
+		text: "Вам сейчас холодно?",
+		variants: [
+			{
+				text: "Да"
+			},
+			{
+				text: "Нет"
+			}
+		],
+		numberInfo: { index: 1, length: 20 }
+	}
 ];
 
-export default function Home() {
-  const [index, setIndex] = useState(0);
-  const [isEnd, setIsEnd] = useState(false);
+function register() {
+	window.widgetUserPayload = {
+		"account_name": "Артем",
+		"h11_account_id": "1",
+		"h11_user_id": "1",
+		"registration_step": "complete"
+	};
+}
 
-  if(!isEnd)
-  return (
-    <div>
-      <Header></Header>
-      <Question
+export default function Home() {
+	const [index, setIndex] = useState(0);
+	const [isEnd, setIsEnd] = useState(false);
+
+	if (!isEnd)
+		return (
+			<div>
+				<Header></Header>
+				{/* <Question
         question={questions[index]}
         numberInfo={{ index: index + 1, length: questions.length }}
         onAnswer={() => {
           if (index + 1  < questions.length) setIndex(index + 1);
           else setIsEnd(true);
         }}
-      ></Question>
-    </div>
-  );
-  else
-  return(<div><div>
-    <Header>
-    </Header>
-    <div className="thank-you">Спасибо за пройденный опрос!</div>
-  </div></div>);
+      ></Question> */}
+				<div onClick={register} className="completeRegistration">
+					<p>Закончить регистрацию</p>
+				</div>
+			</div>
+		);
+	else
+		return (
+			<div>
+				<div>
+					<Header></Header>
+					<div className="thank-you">Спасибо за пройденный опрос!</div>
+				</div>
+			</div>
+		);
 }
